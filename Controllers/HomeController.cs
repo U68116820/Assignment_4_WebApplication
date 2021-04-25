b3bd228ba026bf1b168889ef5106e8da409ff868
siddhi Kulkarni <77395168+U68116820@users.noreply.github.com>
2021-04-25 16:20:39 -0400

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ using System.Net.Http;
 
 namespace Assignment_4_WebApplication.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller 
     {
         private readonly ILogger<HomeController> _logger;
 
